4393aec38844e47cb950239ae9b17612b69adf4f
vicalloy <zbirder@gmail.com>
2017-01-22 19:43:46 +0800

    fix code for pep8

--- 12306.py
+++ 12306.py
@@ -10,7 +10,6 @@ from slackclient import SlackClient
 from conf import SLACK_TOKEN
 from conf import TRAIN_DATES, FROM_STATIONS, TO_STATIONS, TICKET_TYPES, NEED_COUNT
 
-
 INTERVAL_FOR_QUERY = 6
 
 
@@ -18,9 +17,9 @@ def slack_send_message(message):
     sc = SlackClient(SLACK_TOKEN)
 
     sc.api_call(
-      "chat.postMessage",
-      channel="#ticket",
-      text=message
+        "chat.postMessage",
+        channel="#ticket",
+        text=message
     )
 
 
@@ -29,9 +28,29 @@ def send_message(message):
     print(message)
 
 
-class TicketChecker(object):
+def get_train_info(train_date, from_station, to_station='HZH') -> List[Mapping[str, str]]:
+    url = "https://kyfw.12306.cn/otn/leftTicket/queryZ"
+    params = {
+        'leftTicketDTO.train_date': train_date,
+        'leftTicketDTO.from_station': from_station,
+        'leftTicketDTO.to_station': to_station,
+        'purpose_codes': 'ADULT',
+    }
+    r = requests.get(url, params=params, verify=False)
+    train_info_list = []
+    # noinspection PyBroadException
+    try:
+        return_data = r.json()
+        train_info_list = [e['queryLeftNewDTO'] for e in return_data['data']]
+        print('get_train_info succ')
+    except Exception:
+        print('get_train_info fail')
+    return train_info_list
 
-    def __init__(self, train_dates: List[str], from_stations: List[str],
+
+class TicketChecker(object):
+    def __init__(
+            self, train_dates: List[str], from_stations: List[str],
             to_stations: List[str], ticket_types: List[str], need_count: int):
         super(TicketChecker, self).__init__()
         self.train_dates = train_dates
@@ -40,49 +59,19 @@ class TicketChecker(object):
         self.ticket_types = ticket_types
         self.need_count = need_count
 
-    def get_train_info(self, train_date, from_station, to_station='HZH') -> Mapping[str, str]:
-        url = "https://kyfw.12306.cn/otn/leftTicket/queryZ"
-        params = {
-            'leftTicketDTO.train_date': train_date,
-            'leftTicketDTO.from_station': from_station,
-            'leftTicketDTO.to_station': to_station,
-            'purpose_codes': 'ADULT',
-        }
-        r = requests.get(url, params=params, verify=False)
-        train_infos = []
-        try:
-            return_data = r.json()
-            train_infos = [e['queryLeftNewDTO'] for e in return_data['data']]
-            print('get_train_info succ')
-        except:
-            print('get_train_info fail')
-        return train_infos
-
-    def get_left_ticket(self, ticket_type: str, train_info) -> int:
-        """
-        str_count: --,无,有,1,12
-        """
-        str_count = train_info.get('%s_num' % ticket_type)
-        count = 0
-        if str_count == '有':
-            return 9999
-        try:
-            return int(str_count)
-        except:
-            return 0
-
     def get_ok_ticket_types(self, train_info: Mapping[str, str]) -> List[str]:
         ok_ticket_types = []
         for ticket_type in self.ticket_types:
-            left_ticket = self.get_left_ticket(ticket_type, train_info)
+            left_ticket = get_left_ticket(ticket_type, train_info)
             if left_ticket > self.need_count:
                 ok_ticket_types.append(ticket_type)
         return ok_ticket_types
 
-    def send_notification(self, train_info, ticket_type, train_date, from_station, to_station):
+    @staticmethod
+    def send_notification(train_info, ticket_type, train_date, from_station, to_station):
         message = ('日期：{train_date} 类型：{ticket_type} 车次：{train_code} '
-        '开车时间：{start_time}  到达时间：{arrive_time} 余票：{left_ticket} '
-        '历时:{lishi} 出发：{from_station} 达到：{to_station}').format(
+                   '开车时间：{start_time}  到达时间：{arrive_time} 余票：{left_ticket} '
+                   '历时:{lishi} 出发：{from_station} 达到：{to_station}').format(
             train_date=train_date, from_station=from_station, to_station=to_station,
             start_time=train_info['start_time'],
             ticket_type=train_info.get('train_class_name'),
@@ -93,17 +82,29 @@ class TicketChecker(object):
         )
         send_message(message)
 
-
     def check_ticket(self):
         for train_date in self.train_dates:
             for from_station in self.from_stations:
                 for to_station in self.to_stations:
                     time.sleep(INTERVAL_FOR_QUERY)  # sleep
-                    train_infos = self.get_train_info(train_date, from_station, to_station)
-                    for train_info in train_infos:
+                    train_info_list = get_train_info(train_date, from_station, to_station)
+                    for train_info in train_info_list:
                         ok_ticket_types = self.get_ok_ticket_types(train_info)
                         for ok_ticket_type in ok_ticket_types:
                             self.send_notification(train_info, ok_ticket_type, train_date, from_station, to_station)
+
+
+def get_left_ticket(ticket_type: str, train_info) -> int:
+    """
+    str_count: --,无,有,1,12
+    """
+    str_count = train_info.get('%s_num' % ticket_type)
+    if str_count == '有':
+        return 9999
+    try:
+        return int(str_count)
+    except ValueError:
+        return 0
 
 
 ticket_checker = TicketChecker(TRAIN_DATES, FROM_STATIONS, TO_STATIONS, TICKET_TYPES, NEED_COUNT)
